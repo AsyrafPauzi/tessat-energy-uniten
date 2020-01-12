3c517297125af24850629b6d85230f23a10447d2
Masyrafpauzi <acap_sum41@hotmail.com>
2020-01-13 04:34:51 +0800

    first commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,16 +6,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 var jQuery = require("jquery")
 // import jQuery from "jquery";
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
 require('bootstrap');
 
-$('.datepicker').datepicker({
-    format: 'mm/dd/yyyy',
-    startDate: '-3d'
-});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
